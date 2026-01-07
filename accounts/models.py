f10f066048aa65225090eae3e5feea322ebe90fa
amirparsaro <amirparsarouhani@gmail.com>
2026-01-07 19:57:46 +0330

    create db architecture for currency

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 from django.utils import timezone
+from currency.models import Currency
 
 class Status(models.IntegerChoices):
     ACTIVE = 1, "Active"
@@ -32,5 +33,5 @@ class Account(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=Status.choices, default=Status.ACTIVE)
 
-    # currency = models.OneToOneField(Currency, on_delete=models.CASCADE)
+    currency = models.OneToOneField(Currency, on_delete=models.CASCADE, null=True) # delete nullability later
     user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ currency/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Currency(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=128, unique=True)
+    symbol = models.CharField(max_length=10, unique=True)
+    exchange_rate = models.FloatField()
+
+    base_currency = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
